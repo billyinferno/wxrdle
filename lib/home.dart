07efc1e618be27b3b31032d18b3881d013fe34d6
I Gede Adi Martha Ardiana Putra <billytheburninginferno@gmail.com>
2022-03-16 01:17:25 +0800

    Test changes

--- lib/home.dart
+++ lib/home.dart
@@ -35,7 +35,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
 
     // get the max length and answer from settings
-    _maxLength = 5;
+    _maxLength = 6;
     _maxAnswer = 6;
 
     Future.microtask(() async {
