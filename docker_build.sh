390dc9873662be454c5af7ff44da4fd58b9d3024
I Gede Adi Martha <billytheburninginferno@gmail.com>
2025-04-27 17:27:48 +0800

    Push docker image to docker hub repo

--- docker_build.sh
+++ docker_build.sh
@@ -11,3 +11,10 @@ flutter build web --release -t lib/main.dart
 
 # build the docker based on the build
 docker build -t adimartha/wxrdle .
+
+# then tag the latest docker image to the current tag
+docker image tag adimartha/wxrdle:latest adimartha/wxrdle:20250427
+
+# push both of the image to the docker repo
+docker image push adimartha/wxrdle:latest
+docker image push adimartha/my_expense:20250427
